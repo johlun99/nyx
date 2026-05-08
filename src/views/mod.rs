a8097994bdd1180838bd5847bf7e045afaf20d2e
Johan Lundgren <lundgren.johan99@gmail.com>
2026-05-08 13:55:45 +0200

    fix: start all panels hidden and remove unused PanelSlot methods

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -29,33 +29,3 @@ pub enum PanelSlot {
     Bottom,
     Right,
 }
-
-impl PanelSlot {
-    #[allow(dead_code)]
-    pub fn from_config(s: &str) -> Option<Self> {
-        match s {
-            "left" => Some(Self::Left),
-            "bottom" => Some(Self::Bottom),
-            "right" => Some(Self::Right),
-            _ => None,
-        }
-    }
-
-    #[allow(dead_code)]
-    pub fn next(&self) -> Self {
-        match self {
-            Self::Left => Self::Bottom,
-            Self::Bottom => Self::Right,
-            Self::Right => Self::Left,
-        }
-    }
-
-    #[allow(dead_code)]
-    pub fn label(&self) -> &'static str {
-        match self {
-            Self::Left => "left",
-            Self::Bottom => "bottom",
-            Self::Right => "right",
-        }
-    }
-}
